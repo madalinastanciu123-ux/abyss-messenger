fb83042fa3987e856e35a95dfb632095f841095b
madalinastanciu123-ux <madalinastanciu123@gmail.com>
2026-02-21 21:03:11 +0000

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
